9b52102200a5562bb74bf29d2242f98faa181ce7
qiuxuefu376 <qiuxuefu376@pingan.com.cn>
2018-09-04 17:39:20 +0800

    add file interface

--- src/main/java/com/qxf/fileserver/controller/FileController.java
+++ src/main/java/com/qxf/fileserver/controller/FileController.java
@@ -1,10 +1,21 @@
 package com.qxf.fileserver.controller;
 
+
+import com.qxf.fileserver.service.FileUploadService;
+import com.qxf.fileserver.vo.ResponseVO;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 /**
@@ -15,11 +26,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FileController {
 
+    @Autowired
+    private FileUploadService fileUploadService;
 
-    @GetMapping(value = "/attachment")
-    @ApiOperation("查看附件")
-    public void detail() {
+    @ApiOperation(value = "文件读取")
+    @GetMapping(value = "/file/{key}/{name}.{type}")
+    public ResponseEntity<byte[]> read( @PathVariable String key, @PathVariable String name,@PathVariable String type) throws IOException {
 
+        byte[] content = fileUploadService.read(key, name +"."+ type);
+        HttpHeaders headers = new HttpHeaders();
+        if ("PDF".equalsIgnoreCase(type)) {
+            headers.setContentType(MediaType.APPLICATION_PDF);
+        } else {
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        }
+        headers.set("Content-Disposition", "");
+        return new ResponseEntity<>(content, headers, HttpStatus.OK);
     }
+
+    @PostMapping(value = "/attachment")
+    @ApiOperation("附件上传")
+    @ResponseBody
+    @ApiImplicitParams({@ApiImplicitParam(name="file", paramType = "form", dataType="file", value = "附件")})
+    public ResponseVO<String> upload(MultipartFile file) {
+        return ResponseVO.successResponse( fileUploadService.upload(file));
+    }
+
 
 }
